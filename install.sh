8d5b5d7613a92684ea2837f58564694b1c111ee2
Julien ROLLIN <1086565+jrollin@users.noreply.github.com>
2021-04-30 19:51:58 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+install_neovim() {
+  echo "Setting up neovim..." \
+  && rm -rf ~/.config/nvim ~/.fzf \
+  && ln -s $(pwd)/nvim ~/.config/nvim 
+}
+
+install_packages() {
+echo "Installing packages..." \
+    && apt install ripgrep fzf
+}
+
+install_i3() {
+    echo "installing_i3.." \
+        && rm -rf ~/.config/i3 \ 
+        && ln -s $(pwd)/i3 ~/.config/i3  
+}
+
+if [[ -z $1 ]]; then
+  echo -n "This will delete all your previous nvim, Proceed? (y/n)? "
+  read answer
+  if echo "$answer" | grep -iq "^y" ;then
+    echo "Installing dependencies..." \
+    install_neovim \
+    && install_packages \
+    && install_i3 \
+    && echo "Finished installation."
+  fi
+else
+  "install_$1" $1
+fi
+
