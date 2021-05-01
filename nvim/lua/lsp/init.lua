f5dfed99d283402dba4ec3ea0cba658b47fd2fcc
Julien ROLLIN <1086565+jrollin@users.noreply.github.com>
2021-05-01 18:06:49 +0200

    fix nmap error

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -8,23 +8,23 @@ local on_attach = function(client, bufnr)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
 
-    local opts = {noremap= true, silent = true, normal=true } 
+    local opts = {noremap = true, silent = true } 
     -- Keybindings for LSPs
     map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
     map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
     map("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
     map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    map("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    map("<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    map("<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    map("<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    map("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    map("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    map("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    map("<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    map("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    map("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    map("<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    map("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    map("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    map("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    map("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    map("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    map("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    map("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 end
 
 -- typescript
