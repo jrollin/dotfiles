657264ef4dd9d7b61912619349f1e3d0981bb143
jrollin <1086565+jrollin@users.noreply.github.com>
2022-10-02 19:44:46 +0200

    update polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ sleep 1
 echo "---" | tee -a /tmp/polybar1.log 
 polybar mine --config=~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
 
-polybar DP2 --config=~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar DP2 --config=~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
 
 
 
