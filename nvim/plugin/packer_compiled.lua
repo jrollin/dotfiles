f9fa8fd0736b5fdabbab3f6e53a2c0afb383ab0f
jrollin <1086565+jrollin@users.noreply.github.com>
2022-12-06 16:35:20 +0100

    fix rust mason lsp config to activate clippy on save

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -244,6 +244,11 @@ _G.packer_plugins = {
     path = "/home/jrollin/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring",
     url = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring"
   },
+  ["nvim-ts-rainbow"] = {
+    loaded = true,
+    path = "/home/jrollin/.local/share/nvim/site/pack/packer/start/nvim-ts-rainbow",
+    url = "https://github.com/p00f/nvim-ts-rainbow"
+  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "/home/jrollin/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
