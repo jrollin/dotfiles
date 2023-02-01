f7f92fe6e7f86bd5db454cfe8d43246d9b1ecb1c
jrollin <1086565+jrollin@users.noreply.github.com>
2023-02-01 11:21:25 +0100

    auto open tree on vim enter

--- nvim/after/plugin/tree.lua
+++ nvim/after/plugin/tree.lua
@@ -1,1 +1,8 @@
 require("nvim-tree").setup({})
+
+local function open_nvim_tree()
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
