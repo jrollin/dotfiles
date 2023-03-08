98ca551f5757d6c9864446b7dc172f9108613c18
julien <1086565+jrollin@users.noreply.github.com>
2023-03-08 14:36:21 +0100

    update arch packages

--- install_arch.sh
+++ install_arch.sh
@@ -25,7 +25,10 @@ pacman -S \
 
 #xorg intel
 ## Install Intel graphic drivers
-pacman -S xf86-video-intel
+pacman -S \
+    xf86-video-intel \
+    intel-media-driver  
+
 
 # vim
 pacman -S \
@@ -46,7 +49,18 @@ pacman -S \
 
 # sound
 pacman -S \
-  pulseaudio-bluetooth \
-  pulseaudio-alsa \
+  pipewire-{audio,jack,alsa,pulse} \
+  wireplumber \
+  xdg-desktop-portal-gtk \
   pavucontrol
 
+
+# test hardware aceleration :
+# mpv --hwdec=auto PATH/videofile
+# vainfo
+# glxinfo | grep "direct rendering"
+sudo pacman -S \
+    mpv \
+    libva-utils \
+    vdpauinfo \
+    glxinfo  
