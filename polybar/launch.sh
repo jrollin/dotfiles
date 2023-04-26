9a40d1f9f3912a1e3af57e145064bf40cb0c4536
jrollin <1086565+jrollin@users.noreply.github.com>
2023-04-26 16:41:32 +0200

    upate i3, polybar and alacritty conf

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,7 +2,9 @@
 
 # Terminate already running bar instances
 killall -q polybar
-sleep 1
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 echo "---" | tee -a /tmp/polybar1.log 
 polybar mine --config=~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
