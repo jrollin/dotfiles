5c0f8c5792167990199d3e7c41a90b6a938ee98f
jrollin <1086565+jrollin@users.noreply.github.com>
2023-06-15 18:07:21 +0200

    i3: change bg picture

--- i3/startup.sh
+++ i3/startup.sh
@@ -4,7 +4,7 @@
 xbacklight -set 1
 
 # background
-feh --bg-fill ~/.config/pictures/blade-runner.jpg
+feh --bg-fill ~/.config/pictures/city.jpg
 
 ## DPMS monitor setting (standby -> suspend -> off) (seconds)
 xset dpms 300 600 900 
