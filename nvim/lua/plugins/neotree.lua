106399cb1501fc02ab30dc771c4e16ea9121771d
jrollin <1086565+jrollin@users.noreply.github.com>
2025-01-31 10:01:21 +0100

    nvim: show hidden files in neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -6,5 +6,21 @@ return {
     keys = {
       { "<C-f>", "<cmd>Neotree reveal left<CR>", desc = "Reveal file in tree" },
     },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
+    },
   },
 }
