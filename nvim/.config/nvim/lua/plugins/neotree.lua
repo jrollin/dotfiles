4a97f9c1709123459f9369c59aed546a502a6ded
jrollin <1086565+jrollin@users.noreply.github.com>
2025-09-12 17:25:02 +0200

    feat: show hidden files in neotree by default

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -11,7 +11,7 @@ return {
         filtered_items = {
           visible = true,
           show_hidden_count = true,
-          hide_dotfiles = true,
+          hide_dotfiles = false,
           hide_gitignored = false,
           hide_by_name = {
             -- '.git',
@@ -22,39 +22,5 @@ return {
         },
       },
     },
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          commands = {
-            avante_add_files = function(state)
-              local node = state.tree:get_node()
-              local filepath = node:get_id()
-              local relative_path = require("avante.utils").relative_path(filepath)
-
-              local sidebar = require("avante").get()
-
-              local open = sidebar:is_open()
-              -- ensure avante sidebar is open
-              if not open then
-                require("avante.api").ask()
-                sidebar = require("avante").get()
-              end
-
-              sidebar.file_selector:add_selected_file(relative_path)
-
-              -- remove neo tree buffer
-              if not open then
-                sidebar.file_selector:remove_selected_file("neo-tree filesystem [1]")
-              end
-            end,
-          },
-          window = {
-            mappings = {
-              ["oa"] = "avante_add_files",
-            },
-          },
-        },
-      })
-    end,
   },
 }
