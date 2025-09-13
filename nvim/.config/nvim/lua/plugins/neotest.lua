ad30ffb77c4f878298f3acdf44ec14782663be8b
jrollin <1086565+jrollin@users.noreply.github.com>
2025-09-13 23:16:50 +0200

    feat: remove unused plugins

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,7 +1,6 @@
 return {
   { "nvim-neotest/neotest-jest" },
   { "marilari88/neotest-vitest" },
-  { "olimorris/neotest-phpunit" },
   { "nvim-neotest/neotest-python" },
   {
     "nvim-neotest/neotest",
