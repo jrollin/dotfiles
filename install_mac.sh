b3795c154b3b4ef49bcb5a60c89d1658582427fa
jrollin <1086565+jrollin@users.noreply.github.com>
2026-02-22 15:43:38 +0100

    feat(install): bootstrap homebrew if not installed

--- install_mac.sh
+++ install_mac.sh
@@ -1,4 +1,10 @@
-# 
+#!/bin/zsh
+
+# Install homebrew if not present
+if ! command -v brew &>/dev/null; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Install all homebrew packages
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	[[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
